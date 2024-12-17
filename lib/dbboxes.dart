e66f9c02f822acde8ac33d49dfb3f0a285e32c3c
Mubiii <heartchazer23553@gmail.com>
2024-12-17 16:23:20 +0500

    Update dbboxes.dart

--- lib/dbboxes.dart
+++ lib/dbboxes.dart
@@ -4,4 +4,17 @@ import 'package:saving_node_project/db_model.dart';
 class DatabaseBoxes{
   
  static Box<DbModel> getData() => Hive.box<DbModel>('triangleNodes');
+
+ 
+  static deleteAllUser(int key) { 
+
+    // return getData().deleteAll(getData().keys);
+    return getData().clear(); 
+  } 
+
+  static deleteUser(String key){
+    // return getData().deleteAt(getData().keyAt(key));
+    return getData().delete(key);
+  }
+
 }
